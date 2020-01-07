87fce75369c722ab69ae2c46b06bffbf8a94e915
Aurora <46496984+AuroraRW@users.noreply.github.com>
2020-01-07 08:59:50 -0600

    Add files via upload

---
+++ unit test/Class1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TriangleArea;
+using NUnit.Framework;
+
+namespace TriangleAreaTests
+{
+    public class Class1
+    {
+        [TestFixture]
+        public class calculateArea
+        {
+            [Test]
+            public void calculateArea_Base5point98andHeight6point75_Area20point1825()
+            {
+                //Arrange
+                double triangleBase = 5.98;
+                double triangleHeight = 6.75;
+
+                double expectedResult = 20.1825;
+
+                //Act
+                double actualResult = TriangleArea.Program.calculateArea(triangleBase, triangleHeight);
+
+                //Assert
+                Assert.AreEqual(expectedResult, actualResult);
+            }
+            [Test]
+            public void calculateArea_Base6point0andHeight7point0_Area21()
+            {
+                //Arrange
+                double triangleBase = 6.0;
+                double triangleHeight = 7.0;
+
+                double expectedResult = 21;
+
+                //Act
+                double actualResult = TriangleArea.Program.calculateArea(triangleBase, triangleHeight);
+
+                //Assert
+                Assert.AreEqual(expectedResult, actualResult);
+            }
+        }
+    }
+    
+}
---
+++ unit test/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TriangleArea
+{
+   
+   
+        public class Program
+        {
+            static void Main(string[] args)
+            {
+                double b;
+                double h;
+
+            Console.WriteLine("*** This Program will calculate the area of a right angled triangle ***");
+
+                Console.WriteLine("Enter the length of the base: ");
+                b = Convert.ToDouble(Console.ReadLine());
+
+                Console.WriteLine("Enter the height of the triangle: ");
+                h = Convert.ToDouble(Console.ReadLine());
+
+                double area = Program.calculateArea(b, h);
+                Console.WriteLine(area);
+                Console.ReadLine();
+            }
+
+            public static double calculateArea(double b, double h)
+            {
+            
+                return 0.5 * b * h;
+            }
+        }
+   
+}
